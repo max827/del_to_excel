5ba8efc8e0032e46365dac217f00c5b420d79fa8
max827 <942134712@qq.com>
2021-09-05 22:38:35 +0800

    1.1(基本完成，还差解压缩和数据格式问题)

--- getdata(1).py
+++ getdata(1).py
@@ -4,6 +4,8 @@ import xlwt
 import xlrd
 from xlutils.copy import copy
 import re
+import tarfile
+import gzip
 
 
 # 读取文件夹下所有文件
@@ -13,73 +15,110 @@ def del_excel(filePath):
     # 生成Excel
     xls = xlwt.Workbook()
     sheet = xls.add_sheet('sheet1', cell_overwrite_ok=True)
-    x = 0
+    x = 1
     while True:
         line = f.readline()
         if not line:
             break
         for i in range(len(line.split(','))):
             item = re.sub('"', '', line.split(',')[i])
-            sheet.write(x, i, item)
+            sheet.write(x, i+1, formatCell(item))
         x += 1
 
     f.close()
-    xls.save(filePath.split('.')[0]+'.xlsx')
+    xls.save(filePath.split('.')[0] + '.xlsx')
 
 
 def delToExcelWithColumnName(columnFilePath, filePath):
+    #如果没有对应的表头，那就直接造一个新的Excel出来
+    if(columnFilePath == ""):
+        del_excel(filePath)
+        return
+    # 如果有对应的表头，就在表头后面追加内容
     f = open(filePath)
-    # 打开列名所在的工作簿
+    # 打开表头所在的工作簿
     workbook = xlrd.open_workbook(columnFilePath)
-    # sheets = workbook.sheet_names()
     worksheet = workbook.sheet_by_index(0)
     rows_old = worksheet.nrows
     new_wordbook = copy(workbook)
-    new_wordsheet = new_wordbook.get_sheet(0)
+    new_worksheet = new_wordbook.get_sheet(0)
 
-    #追加
+    # 追加
     row = rows_old
     while True:
         line = f.readline()
         if not line:
             break
-        for col in range(0,len(line.split(','))):
+        for col in range(0, len(line.split(','))):
             item = re.sub('"', '', line.split(',')[col])
-            new_wordsheet.write(row, col+1, item)
+            new_worksheet.write(row, col + 1, formatCell(item))
         row += 1
 
     f.close()
     new_wordbook.save(filePath.split('.')[0] + '.xlsx')
 
 
+def unTar(fileName):
+    """
+       解压tar.gz文件
+       :param fname: 压缩文件名
+       :param dirs: 解压后的存放路径
+       :return: bool
+    """
+    try:
+        t = tarfile.open(fileName)
+        names = t.getnames()
+        dirs = fileName + "_files"
+        # 创建文件夹用来存放解压后的文件
+        if os.path.isdir(dirs):
+            pass
+        else:
+            os.mkdir(dirs)
+        for name in names:
+            t.extract(name, dirs)
+        # t.extractall(path = dirs)
+        t.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
-#
-# oslist = os.listdir(filePath)
-#
-# for i in oslist:
-#     f = open(filePath+"\"+i)
-#     line = f.readline()
-#     while line:
-#         print(line)
-#         print(type(line))
-#         line = f.readline()
-#     f.close()
-# excel_path=filePath+'\\'+i
-# data = pd.read_excel(io=excel_path)
-# print(data)
+def getPathDict(delFilesLongPath, colNameLongPath):
+    delFilesPathList = os.listdir(delFilesLongPath)
+    colNamePathList = os.listdir(colNameLongPath)
+    dict = {}
+    for delFilesPath in delFilesPathList:
+        # 如果文件名包含del后缀，再对这个文件进行操作
+        if "del" in delFilesPath:
+            for colNamePath in colNamePathList:
+                # 保证这个文件是表头
+                if "xls" in colNamePath:
+                    colName = colNamePath.split('.')[0]
+                    colPath = colNameLongPath + '\\' + colNamePath
+                    delPath = delFilesLongPath + '\\' + delFilesPath
+                    if colName in delFilesPath:
+                        dict[delPath] = colPath
+                        break
+                    else:
+                        dict[delPath] = ""
+    return dict
 
-# excel_path=filePath+'\\'+oslist[0]
-# data = pd.read_excel(io=excel_path)
-# print(data)
+
+def formatCell(cell):
+    '''
+    将单元格的格式进行转换
+    :param cell: 单元格的值
+    :return: 转换好的单元格值
+    '''
+    newCell = cell
+
+    return newCell
 
 if __name__ == "__main__":
-    filePath = r'C:\Users\Administrator\Desktop\八月\同业\PBC_CLTYCK_20210731_ADD_828.del'
-    # oslist = os.listdir(filePath)
-    # for i in oslist:
-    #     excel_path = filePath + '\\' + i
-    #     # data = pd.read_excel(io=excel_path)
-    #     del_excel(excel_path)
-    #     print(excel_path)
-    #     print(excel_path.split('.')[0]+'.xlsx')
-    columnFilePath = r'C:\Users\Administrator\Desktop\八月\col\column.xls'
-    delToExcelWithColumnName(columnFilePath,filePath)
+    delFilesLongPath = r'C:\Users\admin\Desktop\PBC\delFiles'
+    colNameLongPath = r'C:\Users\admin\Desktop\PBC\ColumnName'
+
+    dict = getPathDict(delFilesLongPath, colNameLongPath)
+    for key, value in dict.items():
+        # print(key+": "+value)
+        delToExcelWithColumnName(value, key)
