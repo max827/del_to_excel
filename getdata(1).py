db2686ced803846ff2fb4b39c0fdc1cbb99f1f5c
max827 <942134712@qq.com>
2021-09-21 22:52:54 +0800

    1.2(单元格数据格式可改变，但是解压缩问题还是没有解决)

--- getdata(1).py
+++ getdata(1).py
@@ -9,7 +9,11 @@ import gzip
 
 
 # 读取文件夹下所有文件
-def del_excel(filePath):
+def del_excel(filePath, targetPath):
+    """
+    :param filePath: del文件路径
+    :return: 无
+    """
     f = open(filePath)
 
     # 生成Excel
@@ -22,17 +26,25 @@ def del_excel(filePath):
             break
         for i in range(len(line.split(','))):
             item = re.sub('"', '', line.split(',')[i])
-            sheet.write(x, i+1, formatCell(item))
+            sheet.write(x, i + 1, item, style=formatCell(item))
         x += 1
 
     f.close()
-    xls.save(filePath.split('.')[0] + '.xlsx')
+
+    # 保存到目标文件夹
+    filename = filePath.split('\\')[-1]
+    xls.save(targetPath + '\\' + filename.split('.')[0] + '.xlsx')
 
 
-def delToExcelWithColumnName(columnFilePath, filePath):
-    #如果没有对应的表头，那就直接造一个新的Excel出来
-    if(columnFilePath == ""):
-        del_excel(filePath)
+def delToExcelWithColumnName(columnFilePath, filePath, targetPath):
+    """
+    :param columnFilePath: 表头的文件路径
+    :param filePath: del文件路径
+    :return: 无
+    """
+    # 如果没有对应的表头，那就直接造一个新的Excel出来
+    if columnFilePath == "":
+        del_excel(filePath, targetPath)
         return
     # 如果有对应的表头，就在表头后面追加内容
     f = open(filePath)
@@ -51,11 +63,51 @@ def delToExcelWithColumnName(columnFilePath, filePath):
             break
         for col in range(0, len(line.split(','))):
             item = re.sub('"', '', line.split(',')[col])
-            new_worksheet.write(row, col + 1, formatCell(item))
+            new_worksheet.write(row, col + 1, item, style=formatCell(item))
         row += 1
 
     f.close()
-    new_wordbook.save(filePath.split('.')[0] + '.xlsx')
+    # 保存到目标文件夹
+    filename = filePath.split('\\')[-1]
+    new_wordbook.save(targetPath + '\\' + filename.split('.')[0] + '.xlsx')
+
+
+def getPathDict(delFilesLongPath, colNameLongPath):
+    delFilesPathList = os.listdir(delFilesLongPath)
+    colNamePathList = os.listdir(colNameLongPath)
+    nameDict = {}
+    for delFilesPath in delFilesPathList:
+        # 如果文件名包含del后缀，再对这个文件进行操作
+        if "del" in delFilesPath and ".gz" not in delFilesPath:
+            for colNamePath in colNamePathList:
+                # 保证这个文件是表头
+                if "xls" in colNamePath:
+                    colName = colNamePath.split('.')[0]
+                    colPath = colNameLongPath + '\\' + colNamePath
+                    delPath = delFilesLongPath + '\\' + delFilesPath
+                    if colName in delFilesPath:
+                        nameDict[delPath] = colPath
+                        break
+                    else:
+                        nameDict[delPath] = ""
+    return nameDict
+
+
+def formatCell(cell):
+    """
+    将单元格的格式进行转换
+    :param cell: 单元格的值
+    :return: 单元格对应的格式
+    """
+    # 数字变成常规，其他变成文本
+    style1 = xlwt.XFStyle()
+    # 如果有小数点就当成数字
+    if '.' in cell:
+        style1.num_format_str = 'general'
+    else:
+        style1.num_format_str = '@'
+
+    return style1
 
 
 def unTar(fileName):
@@ -67,58 +119,40 @@ def unTar(fileName):
     """
     try:
         t = tarfile.open(fileName)
-        names = t.getnames()
-        dirs = fileName + "_files"
-        # 创建文件夹用来存放解压后的文件
-        if os.path.isdir(dirs):
-            pass
-        else:
-            os.mkdir(dirs)
-        for name in names:
-            t.extract(name, dirs)
-        # t.extractall(path = dirs)
-        t.close()
+        t.extractall()
+        # names = t.getnames()
+        # dirs = fileName + "_files"
+        # # 创建文件夹用来存放解压后的文件
+        # if os.path.isdir(dirs):
+        #     pass
+        # else:
+        #     os.mkdir(dirs)
+        # for name in names:
+        #     t.extract(name, dirs)
+        # t.close()
         return True
     except Exception as e:
         print(e)
         return False
 
-def getPathDict(delFilesLongPath, colNameLongPath):
-    delFilesPathList = os.listdir(delFilesLongPath)
-    colNamePathList = os.listdir(colNameLongPath)
-    dict = {}
-    for delFilesPath in delFilesPathList:
-        # 如果文件名包含del后缀，再对这个文件进行操作
-        if "del" in delFilesPath and ".gz" not in delFilesPath:
-            for colNamePath in colNamePathList:
-                # 保证这个文件是表头
-                if "xls" in colNamePath:
-                    colName = colNamePath.split('.')[0]
-                    colPath = colNameLongPath + '\\' + colNamePath
-                    delPath = delFilesLongPath + '\\' + delFilesPath
-                    if colName in delFilesPath:
-                        dict[delPath] = colPath
-                        break
-                    else:
-                        dict[delPath] = ""
-    return dict
 
-
-def formatCell(cell):
-    '''
-    将单元格的格式进行转换
-    :param cell: 单元格的值
-    :return: 转换好的单元格值
-    '''
-    newCell = cell
-
-    return newCell
+def uncompress(filesPath):
+    fileslist = os.listdir(filesPath)
+    for path in fileslist:
+        # 只找压缩文件
+        if 'tar' in path:
+            filepath = filesPath + '\\' + path
+            if os.path.isfile(filepath):
+                unTar(filepath)
 
 if __name__ == "__main__":
-    delFilesLongPath = r'C:\Users\Administrator\Desktop\九月'
-    colNameLongPath = r'C:\Users\Administrator\Desktop\Col'
-
-    dict = getPathDict(delFilesLongPath, colNameLongPath)
-    for key, value in dict.items():
-        print(key+": "+value)
-        delToExcelWithColumnName(value, key)
+    # delFilesLongPath = r'C:\Users\admin\Desktop\PBC\delFiles'
+    # colNameLongPath = r'C:\Users\admin\Desktop\PBC\ColumnName'
+    # targetPath = r'C:\Users\admin\Desktop\456'
+    #
+    # nameDict = getPathDict(delFilesLongPath, colNameLongPath)
+    # for key, value in nameDict.items():
+    #     print(key+": "+value)
+    #     delToExcelWithColumnName(value, key, targetPath)
+    filespath = r'C:\Users\admin\Desktop\PBC\delFiles'
+    uncompress(filespath)
